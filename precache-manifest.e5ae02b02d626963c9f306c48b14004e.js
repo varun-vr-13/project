7a140f0b8879225230f5d35fba1bf2ab1bb18293
varun-vr-13 <varuncool495@gmail.com>
2020-07-15 10:40:33 +0530

    Updates

---
+++ precache-manifest.e5ae02b02d626963c9f306c48b14004e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f357b62d0db072d0e83c245ecb62f273",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "b43c0e4783852477e882",
+    "url": "/project/static/css/main.9a20fe46.chunk.css"
+  },
+  {
+    "revision": "cdce5273a5085f278f7c",
+    "url": "/project/static/js/2.6c093009.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/project/static/js/2.6c093009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43c0e4783852477e882",
+    "url": "/project/static/js/main.4f54634c.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  },
+  {
+    "revision": "5eee94f390f242bb66bd55b5b0c33bdc",
+    "url": "/project/static/media/book-book-pages-college-education-448835.5eee94f3.jpg"
+  },
+  {
+    "revision": "d2271db5b12d938084796df2e4503e85",
+    "url": "/project/static/media/grad.d2271db5.jpg"
+  },
+  {
+    "revision": "25195f885a230ebc40673983de9067c3",
+    "url": "/project/static/media/joshua-newton-wxW6Cp4WmE4-unsplash.25195f88.jpg"
+  },
+  {
+    "revision": "f8bbf8b9c990532b3c798326c82ace7d",
+    "url": "/project/static/media/lifestyle-phrases-in-frames-6149.f8bbf8b9.jpg"
+  },
+  {
+    "revision": "326b2bcea3cd9f75139c884b04a52516",
+    "url": "/project/static/media/pro.326b2bce.jpg"
+  },
+  {
+    "revision": "8e409e424612e3fe39a5633e4ca77cff",
+    "url": "/project/static/media/profile.8e409e42.jpg"
+  },
+  {
+    "revision": "c60068a869f3bd97a4d0184f9d3b6828",
+    "url": "/project/static/media/top-view-photo-of-boy-drawing-on-white-paper-4144097.c60068a8.jpg"
+  },
+  {
+    "revision": "65e1ca867eea0e679dbf049a351ce996",
+    "url": "/project/static/media/top-view-photo-of-people-writing-on-notebook-1557251.65e1ca86.jpg"
+  },
+  {
+    "revision": "77bede32e2efdffb5c65caaddb64cb73",
+    "url": "/project/static/media/var1.77bede32.jpg"
+  },
+  {
+    "revision": "0a2b5fd309ffa2215e8b0eedc014a700",
+    "url": "/project/static/media/you-got-this-lighted-signage-2740954.0a2b5fd3.jpg"
+  }
+]);
